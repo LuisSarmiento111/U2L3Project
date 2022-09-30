ef41cd5cbe6dac1b17ea89ccfd646f6f9630b498
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-09-30 11:40:37 -0400

    Created a new object, George, using the ChatBox class

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -4,6 +4,9 @@ public class U2L3Runner {
     public static void main(String[] args)
     {
         Scanner myObj = new Scanner(System.in);
-
+        ChatBot George = new ChatBot();
+        George.greeting();
+        System.out.println("What would you like to do?");
+        System.out.println("You can:");
     }
 }
